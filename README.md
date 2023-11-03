45dbbc61b762007cd8a43a0458588a657ed24aa8
roraima1986 <96786551+roraima1986@users.noreply.github.com>
2023-11-03 20:39:17 -0300

    subir proyecto

---
+++ README.md
@@ -0,0 +1,21 @@
+# Frontend Mentor - FAQ accordion card
+
+## Overview
+
+### Screenshot
+
+![Design preview for FAQ accordion card coding challenge](./design/faq-accordion-design.jpg)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- JavaScripts
+
+
+## Author
+
+- Website - [https://roraima1986.github.io/faq-accordion.github.io/](https://roraima1986.github.io/faq-accordion.github.io/)
---
+++ scripts.js
@@ -0,0 +1,17 @@
+'use strict'
+
+let accordion = document.querySelectorAll('.accordion');
+
+for (let i=0; i<accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle("active");
+        
+        let panel = this.nextElementSibling;
+        
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    })
+}
